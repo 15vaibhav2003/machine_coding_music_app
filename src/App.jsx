8a353615e2b13f66b3f6c4f900ac87be247a81cf
Vaibhav sharma <vaibhavsharma15eb2003@gmail.com>
2024-03-30 23:41:43 +0530

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import Card from './Components/Card'
+import React from 'react';
+import Card from './Components/Card';
 
 const App = () => {
-  const data=[{Name:'Sanam re',Artist:'KK'},{Name:'Sanam re',Artist:'KK'},{Name:'Sanam re',Artist:'KK'},{Name:'Sanam re',Artist:'KK'},{Name:'Sanam re',Artist:'KK'}
-  ,{Name:'Sanam re',Artist:'KK'}]
-  
+  const data = [
+    { Name: 'Sanam re', Artist: 'KK' },
+    { Name: 'Sanam re', Artist: 'KK' },
+    { Name: 'Sanam re', Artist: 'KK' },
+    
+  ];
+
   return (
-    <div className='w-full h-screen bg-zinc-300 flex items-center justify-center flex-wrap gap-10 gap-y-0'>
-    {data.map((item,index)=>  <Card  key ={index}/>)}
+    <div className='w-full h-screen bg-zinc-300 flex items-center justify-center flex-wrap flex-row gap-4'>
+      {data.map((item, index) => <Card key={index}  />)}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
