61ca9161a96fc2694d8269d7077c84ce49193c90
Vaibhav sharma <vaibhavsharma15eb2003@gmail.com>
2024-03-30 15:12:31 +0530

    morning commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Card from './Components/Card'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='w-full h-screen bg-black'>
+      <Card></Card>
+    </div>
   )
 }
 
