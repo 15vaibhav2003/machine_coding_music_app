0a10c06df12f892902c78449139add713193b3af
Vaibhav sharma <vaibhavsharma15eb2003@gmail.com>
2024-03-30 23:22:21 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import Card from './Components/Card'
 
 const App = () => {
+  const data=[{Name:'Sanam re',Artist:'KK'},{Name:'Sanam re',Artist:'KK'},{Name:'Sanam re',Artist:'KK'},{Name:'Sanam re',Artist:'KK'},{Name:'Sanam re',Artist:'KK'}
+  ,{Name:'Sanam re',Artist:'KK'}]
+  
   return (
-    <div className='w-full h-screen bg-zinc-300 flex items-center justify-center'>
-      <Card></Card>
+    <div className='w-full h-screen bg-zinc-300 flex items-center justify-center flex-wrap gap-10 gap-y-0'>
+    {data.map((item,index)=>  <Card  key ={index}/>)}
     </div>
   )
 }
